bf2933bb38a3dddc255142203881670528740b29
disney50 <36032319+disney50@users.noreply.github.com>
2018-09-26 11:02:25 +0200

    add level1 and level2 as string

--- src/app/models/quest.ts
+++ src/app/models/quest.ts
@@ -4,6 +4,8 @@ export class Quest {
         this.key = key;
         this.title = data.title;
         this.description = data.description;
+        this.level1 = data.level1;
+        this.level2 = data.level2;
         this.max_xp = data.max_xp;
         this.order = data.order;
         this.prerequisites = data.prerequisites;
@@ -12,6 +14,8 @@ export class Quest {
         key: string;
         title: string; 
         description: string;
+        level1: string;
+        level2: string;
         max_xp: string;
         order: number;
         prerequisites: string;
@@ -20,6 +24,8 @@ export class Quest {
         return {
             title: this.title,
             description: this. description,
+            level1: this.level1,
+            level2: this.level2,
             max_xp: this.max_xp,
             order: this.order,
             prerequisites: this.prerequisites
@@ -31,6 +37,8 @@ export class Quest {
 export class QuestData {
     title: string;
     description: string;
+    level1: string;
+    level2: string;
     max_xp: string;
     order: number;
     prerequisites: string;

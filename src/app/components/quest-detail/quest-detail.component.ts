23ad75eaa5a25f301b4b7b3b18bd984fa6a111fc
disney50 <36032319+disney50@users.noreply.github.com>
2018-10-10 12:44:12 +0200

    quest-detail work

--- src/app/components/quest-detail/quest-detail.component.ts
+++ src/app/components/quest-detail/quest-detail.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { GlobalService } from '../../services/global/global.service';
+import { QuestService } from 'src/app/services/quest/quest.service';
+import { Quest } from 'src/app/models/quest';
 
 @Component({
   selector: 'app-quest-detail',
@@ -8,10 +10,15 @@ import { GlobalService } from '../../services/global/global.service';
 })
 export class QuestDetailComponent implements OnInit {
 
-  constructor(public globalService: GlobalService) { }
+  constructor(public globalService: GlobalService, private questService: QuestService) { }
 
   ngOnInit() {
 
+  }
+
+  editQuest(): void {
+    this.questService.editQuest(this.globalService.selectedQuest);
+    this.globalService.selectedQuest = {} as Quest;
   }
 
 }

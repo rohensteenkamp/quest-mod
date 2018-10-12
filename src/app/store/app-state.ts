4618f538083fc5d6a26b453aa5705dce0f030c4a
disney50 <36032319+disney50@users.noreply.github.com>
2018-10-12 11:12:00 +0200

    explorerState to app-state

--- src/app/store/app-state.ts
+++ src/app/store/app-state.ts
@@ -1,13 +1,21 @@
 import { Planet } from "../models/planet";
 import { Quest } from "../models/quest";
+import { Explorer } from "../models/explorer";
 
 export interface AppState {
+
     planetState: {
         planets: Planet[],
         loading: boolean
     }
+
     questState: {
         quests: Quest[],
+        loading: boolean
+    }
+
+    explorerState: {
+        explorers: Explorer[],
         loading: boolean
     }
 }
